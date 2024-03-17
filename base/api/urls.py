fc8581833955dcf13673825e9ac1c97d1c22ab64
Nikhil <sainikhil1258@gmail.com>
2024-03-17 21:05:27 +0530

    Initial commit

---
+++ base/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from .import views
+
+
+
+urlpatterns = [
+    path('', views.getRoutes),
+    path('rooms/', views.getRooms),
+    path('rooms/<str:pk>/', views.getRoom),
+
+]
+
+
